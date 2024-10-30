14ee58caea27d3a08fc61dfc396271ace0ce1e02
shubh-151 <shubhin151@gmail.com>
2024-10-30 10:32:19 +0530

    play with the rotes

--- homework.md
+++ homework.md
@@ -22,3 +22,53 @@
 - Use of regex in routes /a/, ./.*fly$/
 - Reading the query params in the routes
 - Reading the dynamic routes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// order matter for all the routes in nodejs 
+app.use("/users",(req,res)=>{
+    res.send("HAHAHAHAHAHAHAHA")
+});
+
+
+// this will only handle Get call to /user
+app.get("/users",(req,res)=>{
+    res.send({firstName:"Shubham", lastName:"Shukla"})
+});
+
+// this will only handle Get call to /user
+app.post("/users",(req,res)=>{
+    res.send("Data saved in database")
+});
+
+app.delete("/users",(req,res)=>{
+    res.send("Data delete from database")
+});
+
+
+
+// this route will work for all http request get and post etc
+app.use("/test",(req,res)=> {
+    res.send("hello form the  test route");
+})
+
--- src/app.js
+++ src/app.js
@@ -3,34 +3,15 @@ const express = require("express");
 
 const app = express();
 
-
-// order matter for all the routes in nodejs 
-app.use("/users",(req,res)=>{
-    res.send("HAHAHAHAHAHAHAHA")
+app.use("/users", (req, res,next)=>{
+    console.log("Handling the route user!!");
+    //res.send("response!!");
+    next();
+},(req,res)=>{
+    console.log("handling the route user 2!!")
+    res.send("2nd Response!!")
 });
 
-
-// this will only handle Get call to /user
-app.get("/users",(req,res)=>{
-    res.send({firstName:"Shubham", lastName:"Shukla"})
-});
-
-// this will only handle Get call to /user
-app.post("/users",(req,res)=>{
-    res.send("Data saved in database")
-});
-
-app.delete("/users",(req,res)=>{
-    res.send("Data delete from database")
-});
-
-
-
-// this route will work for all http request get and post etc
-app.use("/test",(req,res)=> {
-    res.send("hello form the  test route");
-})
-
-app.listen(3000,()=>{
-    console.log("server is listining on port 3000..");
+app.listen(7000,()=>{
+    console.log("server is listining on port 7000..");
 });
