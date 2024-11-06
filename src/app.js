bddc20009e7f7b779d820c29dc8e7bacfc9132cc
shubh-151 <shubhin151@gmail.com>
2024-11-06 13:16:49 +0530

    Multiple route handler

--- src/app.js
+++ src/app.js
@@ -1,17 +1,36 @@
 const express = require("express");
 
-
 const app = express();
 
-app.use("/user", (req, res,next)=>{
-    console.log("Handling the route user!!");
-    
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user 1!!");
+    //res.send("1st response!!");
     next();
-},(req,res)=>{
-    console.log("handling the route user 2!!")
-    res.send("2nd Response!!")
-});
+  },
+  (req, res, next) => {
+    console.log("handling the route user 2!!");
+    //res.send("2nd Response!!")
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route user 3!!");
+    //res.send("3nd Response!!")
+    next();
+  },
+  [(req, res, next) => {
+    console.log("handling the route user 4!!");
+    //res.send("4nd Response!!")
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route user 5!!");
+    res.send("5nd Response!!");
+    next();
+  }],
+);
 
-app.listen(7000,()=>{
-    console.log("server is listining on port 7000..");
+app.listen(7000, () => {
+  console.log("server is listining on port 7000..");
 });
