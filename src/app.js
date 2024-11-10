edef5b1d60553bf86b58ba13398e2832ec2cf741
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2024-11-10 12:28:02 +0530

    few changes in app.js

--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ app.use("/users", (req, res,next)=>{
     console.log("Handling the route user!!");
     //res.send("response!!");
     next();
-  }],
-);
+  });
 
 app.listen(7000, () => {
   console.log("server is listining on port 7000..");
