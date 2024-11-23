fd31874d3151cfd4f2ac3f370d8bae8fc234b774
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2024-11-23 12:01:44 +0530

    All updated changes

--- src/app.js
+++ src/app.js
@@ -3,20 +3,13 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-<<<<<<< HEAD
-app.use("/user", (req, res)=>{
-    console.log("Handling the route user!!");
-    res.send("response!!");
-   
-=======
 app.post("/signup", async (req, res) => {
   //Creating a new instance of my User model
   const user = new User({
-    firstName: "gAURAV",
-    lastName: "pundir",
-    emailID: "gaurav@gmail.com",
-    password: "gaurav@123",
->>>>>>> a182478a13e7ed2973bb26b5f681bf6a0a80fba2
+    firstName: "Shubham",
+    lastName: "Shukla",
+    emailID: "shubh@gmail.com",
+    password: "shubh@123",
   });
   await user.save();
   res.send("User Added sucessfully")
