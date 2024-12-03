47cc0c2e96cbdff2c262cdc18d0b3c7c259f015b
shubh-151 <shubhin151@gmail.com>
2024-12-03 12:27:30 +0530

    Find my first user behalf of emailID

--- src/app.js
+++ src/app.js
@@ -3,17 +3,49 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
+
+
+//Post our data in database
 app.post("/signup", async (req, res) => {
   //Creating a new instance of my User model
   const user = new User({
-    firstName: "gAURAV",
-    lastName: "pundir",
-    emailID: "gaurav@gmail.com",
-    password: "gaurav@123",
+    firstName: "Shivam",
+    lastName: "Shukla",
+    emailId: "shivam@gmail.com",
+    password: "shivam@123",
   });
   await user.save();
   res.send("User Added sucessfully")
 });
+
+// Get user by email
+app.get("/user", async (req, res) => {
+  // Use req.query.emailId for query parameters
+  const userEmail = req.body.emailId;
+  if (!userEmail) {
+    return res.status(400).send("Email ID is required");
+  }
+  
+  try {
+    const users = await User.find({ emailId: userEmail });
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+
+
+
+
+
+//feed API - GET / feed - get all the users from the database
+app.get("/feed",(req,res)=>{
+
+})
+
 
 connectDB()
   .then(() => {
